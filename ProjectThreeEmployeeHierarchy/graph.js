cc6b4675ca9115fc57b0ca3cbd54a3e34e1b7922
Kevin Dyes <kevin.dyes@wechselgott.com>
2021-01-15 23:31:54 +0100

    Finished course

--- ProjectThreeEmployeeHierarchy/graph.js
+++ ProjectThreeEmployeeHierarchy/graph.js
@@ -8,7 +8,78 @@ const svg = d3.select('.canvas')
 	.attr('height', dims.height + 100);
 
 const graph = svg.append('g')
-	.attr('transform', 'translate(50, 50)');
+	.attr('transform', 'translate(0, 50)');
+
+// data strat
+const stratify = d3.stratify()
+	.id(d => d.name)
+	.parentId(d => d.parent);
+
+const tree = d3.tree()
+	.size([dims.width, dims.height])
+
+// create ordinal scale
+const departmentColour = d3.scaleOrdinal(d3['schemeTableau10']);
+
+// update function
+const update = (data) => {
+
+	// remove current nodes
+	graph.selectAll('.node').remove();
+	graph.selectAll('.link').remove();
+
+	// update ordinal scale domain
+	departmentColour.domain(data.map(d => d.department));
+
+	// get updated root node data
+	const rootNode = stratify(data);
+
+	const treeData = tree(rootNode);
+
+	// get nodes selection & join data
+	const nodes = graph.selectAll('.node')
+		.data(treeData.descendants());
+	
+	// get link selection & join data
+	const links = graph.selectAll('.link')
+		.data(treeData.links());
+
+	// enter new links
+	links.enter()
+		.append('path')
+			.attr('class', 'link')
+			.attr('fill', 'none')
+			.attr('stroke', '#aaa')
+			.attr('stroke-width', 2)
+			.attr('d', d3.linkVertical()
+				.x(d => d.x)
+				.y(d => d.y));
+
+	// create enter node groups
+	const enterNodes = nodes.enter()
+		.append('g')
+			.attr('class', 'node')
+			.attr('transform', d => `translate(${d.x}, ${d.y})`);
+
+	// append rects to enter nodes
+	enterNodes.append('rect')
+		//apply ordinal scale
+		.attr('fill', d => departmentColour(d.data.department))
+		.attr('stroke', '#555')
+		.attr('stroke-width', 2)
+		.attr('height', 50)
+		.attr('width', d => d.data.name.length * 20)
+		.attr('transform', d => {
+			return `translate(${d.data.name.length * -10} , -30)`;
+		});
+
+	// append name text
+	enterNodes.append('text')
+		.attr('text-anchor', 'middle')
+		.attr('fill', 'white')
+		.text(d => d.data.name);
+
+};
 
 // data & firestore hook-up
 var data = [];
@@ -37,5 +108,5 @@ db.collection('employees').onSnapshot(res => {
 				break;
 		}
 	});
-
+	update(data);
 });
